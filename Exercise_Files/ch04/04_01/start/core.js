1fafaca5f95c415adf768e2507a2ed77a434293e
JillKlatt <klatt.jill@gmail.com>
2021-10-14 17:48:59 -0400

    Chapter three

---
+++ Exercise_Files/ch04/04_01/start/core.js
@@ -0,0 +1,18 @@
+const path = require("path")
+
+const dirUploads = path.join(__dirname, 'wwww', 'files', 'uploads')
+console.log(dirUploads)
+
+const util = require('util')
+
+util.log( path.basename(__filename) )
+
+util.log(` ^ The name of the current file`)
+
+const v8 = require('v8')
+
+util.log(v8.getHeapStatistics())
+
+const {log} = require('util')
+
+log("Sup")
